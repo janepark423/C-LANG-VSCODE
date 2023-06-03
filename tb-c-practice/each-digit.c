d4d13d5954a489011641f00f4d0e76084c663732
jain <janepark423@gmail.com>
2023-06-04 01:16:36 +0900

    commit

---
+++ tb-c-practice/each-digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int numVal = 0b1010;
+    int res = 0;
+ 
+    // while (numVal != 0)
+    // {
+    //     printf("%d ", numVal % 10);
+ 
+    //     numVal /= 10;
+    // }
+ 
+    res = (numVal >> 1);
+    printf("%d\r\n",res);
+
+    return 0;
+}
