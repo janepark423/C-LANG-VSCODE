2fedf62071d36fe1678926dd34e8ae90eb06cc7d
jain <janepark423@gmail.com>
2023-06-07 01:48:53 +0900

    commit

--- tb-c-practice/homework-about-bitwise/homework.c
+++ tb-c-practice/homework-about-bitwise/homework.c
@@ -20,6 +20,27 @@ int IP14 = 1;
 int IP15 = 1;
 int IP16 = 1;
 
+int OP1 = 1;
+int OP2 = 1;
+int OP3 = 1;
+int OP4 = 1;
+int OP5 = 1;
+int OP6 = 1;
+int OP7 = 1;
+int OP8 = 1;
+int OP9 = 1;
+int OP10 = 1;
+int OP11 = 1;
+int OP12 = 1;
+int OP13 = 1;
+int OP14 = 1;
+int OP15 = 1;
+int OP16 = 1;
+
+
+
+
+
 
 
 void PrintBits(int a_data)
@@ -38,8 +59,10 @@ void PrintBits(int a_data)
         //                      5번째 (a_data & 0000 0100 ) >> 2
         //                      6번째 (a_data & 0000 0010 ) >> 1
         //                      7번째 (a_data & 0000 0001 ) >> 0
+    
     }
     printf("\n");  // 줄바꿈을 한다.
+
 }
 
 void main(void)
@@ -47,38 +70,54 @@ void main(void)
     
         //만약 IP네트 값이 1이면 해당 CN가 연결된 것
     
-        if (IP1 == 1)
+        if (IP1 == 0)
             val = val | 0b0000000000000001; // 16진수 : 0x0001
-        if (IP2 == 1)
+            OP1 =1;
+        if (IP2 == 0)
             val = val | 0b0000000000000010; // 16진수 : 0x0002
-        if (IP3 == 1)
+            OP2 =1;
+        if (IP3 == 0)
             val = val | 0b0000000000000100; // 16진수 : 0x0004
-        if (IP4 == 1)
+            OP3 =1;
+        if (IP4 == 0)
             val = val | 0b0000000000001000; // 16진수 : 0x0008
-        if (IP5 == 1)
+            OP4 =1;
+        if (IP5 == 0)
             val = val | 0b0000000000010000; // 16진수 : 0x0010
-        if (IP6 == 1)
+            OP5 =1;
+        if (IP6 == 0)
             val = val | 0b0000000000100000; // 16진수 : 0x0004
-        if (IP7 == 1)
+            OP6 =1;
+        if (IP7 == 0)
             val = val | 0b0000000001000000; // 16진수 : 0x0004
-        if (IP8 == 1)
+            OP7 =1;
+        if (IP8 == 0)
             val = val | 0b0000000010000000; // 16진수 : 0x0004
-        if (IP9 == 1)
+            OP8 =1;
+        if (IP9 == 0)
             val = val | 0b0000000100000000; // 16진수 : 0x0004
-        if (IP10 == 1)
+            OP9 =1;
+        if (IP10 == 0)
             val = val | 0b0000001000000000; // 16진수 : 0x0004
-        if (IP11 == 1)
+            OP10 =1;
+        if (IP11 == 0)
             val = val | 0b0000010000000000; // 16진수 : 0x0004
-        if (IP12 == 1)
+            OP11 =1;
+        if (IP12 == 0)
             val = val | 0b0000100000000000; // 16진수 : 0x0004
-        if (IP13 == 1)
+            OP12 =1;
+        if (IP13 == 0)
             val = val | 0b0001000000000000; // 16진수 : 0x0004
-        if (IP14 == 1)
+            OP13 =1;
+        if (IP14 == 0)
             val = val | 0b0010000000000000; // 16진수 : 0x0004
-        if (IP15 == 1)
+            OP14 =1;
+        if (IP15 == 0)
             val = val | 0b0100000000000000; // 16진수 : 0x0004
-        if (IP16 == 1)
+            OP15 =1;
+        if (IP16 == 0)
             val = val | 0b1000000000000000; // 16진수 : 0x0004
+            OP16 =1;
         PrintBits(val);
  
 }
